9e4fe6cdfac16b89ea0512945ccd6a1cf3b65fc7
Marco Bacci <marcobacci94@gmail.com>
2025-09-05 17:37:50 +0200

    feat: create state var and add axios call to get products list

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,22 @@
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = () => {
+    axios.get("https://fakestoreapi.com/products").then((response) => {
+      setProducts(response.data);
+      console.log(response.data)
+    });
+  };
+
+  useEffect(fetchProducts, []);
+
   return (
     <div className="container">
       <h1>prodotti</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
