336b036d08bb19733a3bdae04929096f3edc8d9f
Marco Bacci <marcobacci94@gmail.com>
2025-09-06 15:43:35 +0200

    upd: style product page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -21,14 +21,14 @@ const Products = () => {
         {products.map((product) => {
           return (
             <div className="col-4" key={product.id}>
-              <div className="card p-3 h-100" style={{ width: "18rem" }}>
+              <div className="card p-3 h-100 bg-body-secondary" style={{ width: "18rem" }}>
                 <img src={product.image} className="card-img-top" alt="" />
                 <div className="card-body">
                   <h5 className="card-title">{product.title}</h5>
                   <p className="card-text">
-                    category: {product.category} 
+                    <span className="text-danger fw-bold">category:</span> {product.category} 
                   </p>
-                  <p>price: {product.price}$</p>
+                  <p><span className="text-success fw-bold">price:</span> {product.price}$</p>
                   {/* <p>{product.description}</p> */}
                 </div>
               </div>
