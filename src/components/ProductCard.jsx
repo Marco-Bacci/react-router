9008ca335b909ae0cb48ed08012c3cee9709b511
Marco Bacci <marcobacci94@gmail.com>
2025-09-08 21:38:28 +0200

    fix: upd style for DetailProduct

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -15,13 +15,8 @@ const ProductCard = ({info}) => {
         <img src={image} className="card-img-top" alt="" />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
-          <p className="card-text">
-            <span className="text-danger fw-bold">category:</span> {category}
-          </p>
-          <p>
-            <span className="text-success fw-bold">price:</span> {price}$
-          </p>
-          <Link to={`/products/${id}`}> Dettagli Prodotto </Link>
+          
+          <Link className="btn btn-primary mt-2" to={`/products/${id}`}> Dettagli Prodotto </Link>
         </div>
       </div>
     </div>
--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -3,30 +3,27 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 const DetailProduct = () => {
   const { id } = useParams();
-  const [product, setProduct] = useState({})
+  const [product, setProduct] = useState({});
 
-  useEffect(()=>{
-    axios.get(`https://fakestoreapi.com/products/${id}`).then((resp)=>{
-      setProduct(resp.data)
-    })
-  },[])
+  useEffect(() => {
+    axios.get(`https://fakestoreapi.com/products/${id}`).then((resp) => {
+      setProduct(resp.data);
+    });
+  }, []);
 
   return (
     <div className="container">
       <div className="row">
-        <div className="col-12">
+        <div className="col-12 text-center p-3">
           <h3>{product.title}</h3>
-          <img src={product.image} alt="image" />
-          <p>Category: {product.category}</p>
-          <p>Price: {product.price}$</p>
-          <p>{product.description}</p>
-          
         </div>
+        <img src={product.image} alt="image" />
+        <p className="text-center"><span className="text-danger fw-bold">Category:</span>{product.category} <span className="text-success fw-bold">Price:</span> {product.price}$ </p>
+        
+        <p>{product.description}</p>
       </div>
     </div>
   );
 };
 
 export default DetailProduct;
-
-
